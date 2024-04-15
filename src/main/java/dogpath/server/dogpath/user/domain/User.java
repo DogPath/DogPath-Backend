23f6fbdedf78c19830b28d22d49e6fe5e8fa86e3
Han Ju Kim <hjkim2902@gmail.com>
2024-04-15 14:03:43 +0900

    feat : Add User Domain

---
+++ src/main/java/dogpath/server/dogpath/user/domain/User.java
@@ -0,0 +1,31 @@
+package dogpath.server.dogpath.user.domain;
+
+import dogpath.server.dogpath.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String kakaoUserId;
+
+    @Column(length = 25)
+    private String nickname;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "breed_id")
+    private Breed breed;
+}
