57432a7534540b6086b97e2c2bd30c2aa17dd25f
Han Ju Kim <hjkim2902@gmail.com>
2024-04-15 16:27:58 +0900

    fix : Entity 어노테이션 제거

--- src/main/java/dogpath/server/dogpath/global/domain/BaseEntity.java
+++ src/main/java/dogpath/server/dogpath/global/domain/BaseEntity.java
@@ -1,15 +1,11 @@
 package dogpath.server.dogpath.global.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-@Entity
 @MappedSuperclass
 public abstract class BaseEntity {
 
