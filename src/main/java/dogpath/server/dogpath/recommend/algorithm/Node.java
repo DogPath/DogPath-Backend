d9d857f45adf56379cdd2c0b4929c4bdb175689a
lisiant <wkdxod321@gmail.com>
2024-05-03 14:48:10 +0900

    Feat: Create Node class

--- src/main/java/dogpath/server/dogpath/recommend/algorithm/Node.java
+++ src/main/java/dogpath/server/dogpath/recommend/algorithm/Node.java
@@ -1,23 +1,45 @@
-package dogpath.server.dogpath.recommend.service;
+package dogpath.server.dogpath.recommend.algorithm;
 
 import lombok.Getter;
+import lombok.ToString;
 
-import java.awt.*;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 
 @Getter
+@ToString
 public class Node {
+
+    private static final double LAT_MOVE_50M = 0.00045050;
+    private static final double LNG_MOVE_50M = 0.00056597;
     Point2D.Double centerPoint;
-    Rectangle2D.Double area;
-    double score = 0.0;
-    double heuristicDistance = 0.0;
+    Point2D.Double leftTopPoint;
+    Point2D.Double rightTopPoint;
+    Point2D.Double leftBottomPoint;
+    Point2D.Double rightBottomPoint;
+    double score;
+    double heuristicDistance;
 
-    Node(double centerLat, double centerLng) {
-        centerPoint = new Point2D.Double();
-        area = new Rectangle2D.Double();
+    // 중심 좌표를 생성자에서 입력 받으면 100m * 100m 노드 생성
+    Node(double lat, double lng) {
+        calculateNodeArea(lat, lng);
+        this.score = 0.0;
+        this.heuristicDistance = 0.0;
+    }
 
-        centerPoint.setLocation(centerLat, centerLng);
-        
+    private void calculateNodeArea(double centerLat, double centerLng) {
+        this.centerPoint = new Point2D.Double(centerLat, centerLng);
+
+        // 중심으로부터 북서쪽: 위도 증가, 경도 감소
+        this.leftTopPoint = new Point2D.Double(centerLat + LAT_MOVE_50M, centerLng - LNG_MOVE_50M);
+
+        // 중심으로부터 북동쪽: 위도 증가, 경도 증가
+        this.rightTopPoint = new Point2D.Double(centerLat + LAT_MOVE_50M, centerLng + LNG_MOVE_50M);
+
+        // 중심으로부터 남서쪽: 위도 감소, 경도 감소
+        this.leftBottomPoint = new Point2D.Double(centerLat - LAT_MOVE_50M, centerLng - LNG_MOVE_50M);
+
+        // 중심으로부터 남동쪽: 위도 감소, 경도 증가
+        this.rightBottomPoint = new Point2D.Double(centerLat - LAT_MOVE_50M, centerLng + LNG_MOVE_50M);
+
     }
 }
