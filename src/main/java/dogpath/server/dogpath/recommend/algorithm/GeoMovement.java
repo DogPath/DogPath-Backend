5dc91fbeaea90a8d8ad9fdfeb09246bc2a2b8c96
lisiant <wkdxod321@gmail.com>
2024-05-03 18:12:41 +0900

    Feat: Change Enum values
    GeoMovement: 위도, 경도 변환값 수정
    WalkLength: Km 단위 -> 큰 격자에서 한 변의 격자 개수로 변경

--- src/main/java/dogpath/server/dogpath/recommend/algorithm/GeoMovement.java
+++ src/main/java/dogpath/server/dogpath/recommend/algorithm/GeoMovement.java
@@ -2,8 +2,8 @@ package dogpath.server.dogpath.recommend.algorithm;
 
 public enum GeoMovement {
 
-    MOVE_50M(0.000450499, 0.000565969),
-    MOVE_100M(0.000900998, 0.001131938);
+    MOVE_50M(0.0004504995306159225, 0.0005659693923752604),
+    MOVE_100M(0.0009009983061231845, 0.0011319387847505207);
 
     private final double latChangeVal;
     private final double lngChangeVal;
--- src/main/java/dogpath/server/dogpath/recommend/algorithm/WalkLength.java
+++ src/main/java/dogpath/server/dogpath/recommend/algorithm/WalkLength.java
@@ -1,13 +1,13 @@
 package dogpath.server.dogpath.recommend.algorithm;
 
 public enum WalkLength {
-    SHORT(1),
-    MEDIUM(2),
-    LONG(4);
+    SHORT(10),
+    MEDIUM(20),
+    LONG(40);
 
     private final int value;
 
-    private WalkLength(int value){
+    WalkLength(int value){
         this.value = value;
     }
 
