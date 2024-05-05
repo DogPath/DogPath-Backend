d624fdd162cc3d094632593fa99a175ae6cef0b2
lisiant <wkdxod321@gmail.com>
2024-05-05 23:05:11 +0900

    Feat: Add `@Getter` annotation at BaseDataEntity

--- src/main/java/dogpath/server/dogpath/recommend/domain/BaseDataEntity.java
+++ src/main/java/dogpath/server/dogpath/recommend/domain/BaseDataEntity.java
@@ -2,8 +2,10 @@ package dogpath.server.dogpath.recommend.domain;
 
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.locationtech.jts.geom.Point;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseDataEntity {
 
