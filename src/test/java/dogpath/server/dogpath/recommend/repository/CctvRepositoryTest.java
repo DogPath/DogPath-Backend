d369a2e1544153eaf7754f9987ebb3d5f0fffaee
lisiant <wkdxod321@gmail.com>
2024-05-06 00:21:16 +0900

    Chore: remove unused import statements at Test

--- src/test/java/dogpath/server/dogpath/recommend/repository/CctvRepositoryTest.java
+++ src/test/java/dogpath/server/dogpath/recommend/repository/CctvRepositoryTest.java
@@ -2,18 +2,13 @@ package dogpath.server.dogpath.recommend.repository;
 
 
 import dogpath.server.dogpath.recommend.domain.Cctv;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
