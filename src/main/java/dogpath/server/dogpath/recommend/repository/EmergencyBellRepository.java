7ff9b2f3d1c55b6af45dd04c90f35dc4efdf8eb0
lisiant <wkdxod321@gmail.com>
2024-05-06 00:15:53 +0900

    Feat: Add Repository
    - MySQL 데이터와 연동
    - Spring Data JPA

---
+++ src/main/java/dogpath/server/dogpath/recommend/repository/EmergencyBellRepository.java
@@ -0,0 +1,8 @@
+package dogpath.server.dogpath.recommend.repository;
+
+import dogpath.server.dogpath.recommend.domain.EmergencyBell;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmergencyBellRepository extends JpaRepository<EmergencyBell, Long> {
+
+}
