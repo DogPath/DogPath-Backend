e3a2b8f6039111f1a3d554166ebec11fb5b8056d
Han Ju Kim <hjkim2902@gmail.com>
2024-05-13 21:26:08 +0900

    feat : Create AllowanceDistance Enum Class

---
+++ src/main/java/dogpath/server/dogpath/domain/path/algorithm/enums/AllowanceDistance.java
@@ -0,0 +1,22 @@
+package dogpath.server.dogpath.domain.path.algorithm.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AllowanceDistance {
+    /*
+        산책 거리에 따른 허용 범위를 관리하는 Enum 클래스
+        짧은 산책 -> 10% / 중간 산책 -> 10% / 긴 산책 -> 10% (조절가능)
+     */
+    SHORT(0.1),
+    MEDIUM(0.1),
+    LONG(0.1);
+
+    private double range;
+
+    public static AllowanceDistance ofWalkLength(WalkLength walkLength) {
+        return AllowanceDistance.valueOf(walkLength.name());
+    }
+}
