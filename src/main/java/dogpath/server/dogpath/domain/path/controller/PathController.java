c624bf39b0f13bb0c4c96102c6639238de52a6f3
Han Ju Kim <hjkim2902@gmail.com>
2024-05-13 21:28:40 +0900

    feat : 경로 API 컨트롤러 구조 생성

---
+++ src/main/java/dogpath/server/dogpath/domain/path/controller/PathController.java
@@ -0,0 +1,46 @@
+package dogpath.server.dogpath.domain.path.controller;
+
+import dogpath.server.dogpath.domain.path.dto.FindRoutingReq;
+import dogpath.server.dogpath.domain.path.dto.FindRoutingRes;
+import dogpath.server.dogpath.domain.path.service.PathService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/path")
+@RequiredArgsConstructor
+public class PathController {
+
+    private final PathService pathService;
+
+    @PostMapping("/routing")
+    public ResponseEntity<List<FindRoutingRes>> getRoute(@RequestBody FindRoutingReq findRoutingReq) throws IOException {
+        return ResponseEntity.ok(pathService.findRoute(findRoutingReq));
+    }
+
+    @PostMapping("/records")
+    public void getAllRecords(){
+        return;
+    }
+
+    @DeleteMapping("/records/{recordId}")
+    public void deleteRecord(){
+        return;
+    }
+
+    @GetMapping("/records/{recordId}")
+    public void getRecord(){
+        return;
+    }
+}
