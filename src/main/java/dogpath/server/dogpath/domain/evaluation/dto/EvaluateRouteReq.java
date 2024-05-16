7ca2b308133535734465b34c1bb84455d0e4536a
Han Ju Kim <hjkim2902@gmail.com>
2024-05-16 17:16:43 +0900

    feat : dto 생성

---
+++ src/main/java/dogpath/server/dogpath/domain/evaluation/dto/EvaluateRouteReq.java
@@ -0,0 +1,12 @@
+package dogpath.server.dogpath.domain.evaluation.dto;
+
+import lombok.Data;
+
+@Data
+public class EvaluateRouteReq {
+    private Long userId;
+    private double distance;
+    private String time;
+    private String routeImg;
+    private String evaluations;
+}
