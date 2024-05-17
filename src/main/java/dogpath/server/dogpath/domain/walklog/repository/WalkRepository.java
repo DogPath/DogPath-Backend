75f95ddae4661c4283ab325ef33970c553ccabd0
lisiant <wkdxod321@gmail.com>
2024-05-18 00:24:37 +0900

    Rename: rename directory walklog -> walk

--- src/main/java/dogpath/server/dogpath/domain/walklog/repository/WalkRepository.java
+++
@@ -1,9 +0,0 @@
-package dogpath.server.dogpath.domain.walklog.repository;
-
-import dogpath.server.dogpath.domain.walklog.domain.Walk;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WalkRepository extends JpaRepository<Walk, Long> {
-}
