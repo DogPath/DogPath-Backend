28c9ea63b0f5fb7d3053dfb20b2742d6092599f8
lisiant <wkdxod321@gmail.com>
2024-05-18 20:20:17 +0900

    Feat: Add WalkRepository findPathRecordById

--- src/main/java/dogpath/server/dogpath/domain/walk/repository/WalkRepository.java
+++ src/main/java/dogpath/server/dogpath/domain/walk/repository/WalkRepository.java
@@ -1,13 +1,18 @@
 package dogpath.server.dogpath.domain.walk.repository;
 
 import dogpath.server.dogpath.domain.walk.domain.Walk;
+import dogpath.server.dogpath.domain.walk.dto.PathRecordDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WalkRepository extends JpaRepository<Walk, Long> {
     @Query("SELECT a FROM Walk a JOIN FETCH a.user u WHERE u.id = :user_id")
     List<Walk> findPathRecordsById(@Param("user_id") Long userId);
+
+    @Query("SELECT a FROM Walk a JOIN FETCH a.user u WHERE u.id = :user_id AND a.id = :walk_id")
+    Optional<PathRecordDTO> findPathRecordById(@Param("walk_id") Long walkId, @Param("user_id") Long userId);
 }
