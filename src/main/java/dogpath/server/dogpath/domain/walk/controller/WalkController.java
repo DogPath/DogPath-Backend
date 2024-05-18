a6b7f1a9166d9366d7cd6d2e65d52830f13e5ed7
lisiant <wkdxod321@gmail.com>
2024-05-18 20:20:49 +0900

    Feat: add GET, DELETE api

--- src/main/java/dogpath/server/dogpath/domain/walk/controller/WalkController.java
+++ src/main/java/dogpath/server/dogpath/domain/walk/controller/WalkController.java
@@ -1,10 +1,14 @@
 package dogpath.server.dogpath.domain.walk.controller;
 
+import dogpath.server.dogpath.domain.walk.dto.GetPathRecordRequest;
+import dogpath.server.dogpath.domain.walk.dto.GetPathRecordResponse;
 import dogpath.server.dogpath.domain.walk.dto.GetPathRecordsResponse;
 import dogpath.server.dogpath.domain.walk.service.WalkService;
 import dogpath.server.dogpath.global.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -19,6 +23,22 @@ public class WalkController {
     public BaseResponse<GetPathRecordsResponse> getPathRecords(@RequestParam Long userId) {
         log.debug("[WalkController.getPathRecords]");
         return new BaseResponse<>(walkService.getPathRecordsByUserId(userId));
+    }
+
+    // TODO: 예외처리
+    @DeleteMapping("/{record_id}")
+    public ResponseEntity deletePathRecord(@PathVariable("record_id") Long recordId) {
+        log.debug("[WalkController.deletePathRecord]");
+        HttpStatus httpStatus = walkService.deletePathRecordById(recordId);
+        return ResponseEntity.status(httpStatus.value())
+                .build();
+    }
+
+
+    @GetMapping("/{record_id}")
+    public BaseResponse<GetPathRecordResponse> getPathRecordById(@RequestBody GetPathRecordRequest request) {
+        log.debug("[WalkController.getPathRecordById]");
+        return new BaseResponse<>(walkService.getPathRecord(request));
     }
 
 
--- src/main/java/dogpath/server/dogpath/domain/walk/service/WalkService.java
+++ src/main/java/dogpath/server/dogpath/domain/walk/service/WalkService.java
@@ -1,12 +1,23 @@
 package dogpath.server.dogpath.domain.walk.service;
 
+import dogpath.server.dogpath.domain.evaluation.domain.Evaluation;
+import dogpath.server.dogpath.domain.evaluation.domain.WalkEvaluation;
+import dogpath.server.dogpath.domain.evaluation.domain.WalkEvaluationId;
+import dogpath.server.dogpath.domain.evaluation.repository.EvaluationRepository;
+import dogpath.server.dogpath.domain.evaluation.repository.WalkEvaluationRepository;
+import dogpath.server.dogpath.domain.walk.dto.GetPathRecordRequest;
+import dogpath.server.dogpath.domain.walk.dto.GetPathRecordResponse;
 import dogpath.server.dogpath.domain.walk.dto.GetPathRecordsResponse;
 import dogpath.server.dogpath.domain.walk.dto.PathRecordDTO;
 import dogpath.server.dogpath.domain.walk.repository.WalkRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -14,6 +25,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class WalkService {
     private final WalkRepository walkRepository;
+    private final WalkEvaluationRepository walkEvaluationRepository;
+    private final EvaluationRepository evaluationRepository;
 
     public GetPathRecordsResponse getPathRecordsByUserId(Long userId) {
         log.debug("[WalkService.getPathRecordsByUserId]");
@@ -22,6 +35,51 @@ public class WalkService {
                         .stream()
                         .map(PathRecordDTO::new)
                         .collect(Collectors.toList()));
+    }
+
+
+    public GetPathRecordResponse getPathRecord(GetPathRecordRequest request) {
+        log.debug("[WalkService.getPathRecord]");
+        PathRecordDTO pathRecord = walkRepository.findPathRecordById(request.getWalkId(), request.getUserId())
+                .orElseThrow(IllegalArgumentException::new);
+        List<WalkEvaluation> walkEvaluations = walkEvaluationRepository.findByWalkId(pathRecord.getWalkId());
+        String evaluationStr = String.join(",", getEvaluationList(walkEvaluations));
+        pathRecord.setEvaluations(evaluationStr);
+
+        return new GetPathRecordResponse(pathRecord);
+    }
+
+    private ArrayList<String> getEvaluationList(List<WalkEvaluation> walkEvaluations) {
+        ArrayList<String> eval = new ArrayList<>();
+
+        for (WalkEvaluation walkEvaluation : walkEvaluations) {
+            Evaluation evaluation = evaluationRepository.findById(walkEvaluation.getEvaluation().getId())
+                    .orElseThrow(IllegalArgumentException::new);
+            String title = evaluation.getTitle();
+            eval.add(title);
+        }
+        return eval;
+    }
+
+
+    // TODO: 테스트 기능 추가, 예외 처리
+    @Transactional
+    public HttpStatus deletePathRecordById(Long walkId) {
+
+        // WalkEvaluation 가져오기
+        List<WalkEvaluation> walkEvaluations = walkEvaluationRepository.findByWalkId(walkId);
+
+        for (WalkEvaluation walkEvaluation : walkEvaluations) {
+            Long evalId = walkEvaluation.getEvaluation().getId();
+
+            // evaluation, walkEvaluation 삭제
+            evaluationRepository.deleteById(evalId);
+            walkEvaluationRepository.deleteById(new WalkEvaluationId(walkId, evalId));
+
+        }
+        walkRepository.deleteById(walkId);
+
+        return HttpStatus.OK;
     }
 
 }
