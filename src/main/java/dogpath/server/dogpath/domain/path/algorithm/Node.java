39c444677177843811cadfea48f2e02d7e40cda4
Han Ju Kim <hjkim2902@gmail.com>
2024-05-20 14:27:45 +0900

    feat : Node 별 휴리스틱 알고리즘 값 연산 메소드 추가

--- src/main/java/dogpath/server/dogpath/domain/path/algorithm/Node.java
+++ src/main/java/dogpath/server/dogpath/domain/path/algorithm/Node.java
@@ -2,9 +2,14 @@ package dogpath.server.dogpath.domain.path.algorithm;
 
 import dogpath.server.dogpath.domain.path.algorithm.enums.GeoMovement;
 import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.geo.Point;
 
+import java.util.Objects;
 
+
+@Slf4j
 @Getter
 public class Node {
     Point centerPoint;
@@ -53,5 +58,56 @@ public class Node {
         // 중심으로부터 남동쪽: 위도 감소, 경도 증가
         this.bottomRightPoint = new Point(centerLat - GeoMovement.MOVE_50M.getLatChangeVal(), centerLng + GeoMovement.MOVE_50M.getLngChangeVal());
 
+    }
+
+    public double calculateEuclidean(Node node) {
+        Point centerPoint1 = this.centerPoint;
+        Point centerPoint2 = node.centerPoint;
+
+        double x = Math.pow(centerPoint1.getX() - centerPoint2.getX(), 2);
+        double y = Math.pow(centerPoint1.getY() - centerPoint2.getY(), 2);
+        return Math.sqrt(x + y);
+    }
+
+    //휴리스틱 값
+    public double getFValue(Node destination) {
+        this.heuristicDistance = getValueG() + getValueH(destination);
+        return this.heuristicDistance;
+    }
+
+    //g(n) 값
+    private double getValueG(){
+        return -1 * score;
+    }
+
+    //h(n) 값
+    private double getValueH(Node destination){
+        return this.calculateEuclidean(destination);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return boardX == node.boardX && boardY == node.boardY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(boardX, boardY);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "boardX=" + boardX +
+                ", boardY=" + boardY +
+                ", centerPoint=" + centerPoint +
+                ", score=" + score +
+                ", heuristicDistance=" + heuristicDistance +
+                ", isVisited=" + isVisited +
+                ", isPassingNode=" + isPassingNode +
+                "}\n";
     }
 }
