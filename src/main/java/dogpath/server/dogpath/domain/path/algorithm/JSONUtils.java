be343bb2efaf0f225664b86243148dcba2aa02d2
Han Ju Kim <hjkim2902@gmail.com>
2024-05-20 14:25:45 +0900

    feat : JSON Parsing method 추가

--- src/main/java/dogpath/server/dogpath/domain/path/algorithm/JSONUtils.java
+++ src/main/java/dogpath/server/dogpath/domain/path/algorithm/JSONUtils.java
@@ -1,13 +1,33 @@
 package dogpath.server.dogpath.domain.path.algorithm;
 
 import com.squareup.okhttp.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+
+@Slf4j
 public class JSONUtils {
-    public static double parseDistanceFromResponse(Response response) {
-        return 0.1;
+    public static long parseDistanceFromResponse(String response) throws IOException, ParseException {
+        return getTotalFeature(response, "totalDistance");
     }
 
-    public static long parseTimeFromResponse(Response response) {
-        return 1L;
+    public static long parseTimeFromResponse(String response) throws IOException, ParseException {
+        return getTotalFeature(response, "totalTime");
+    }
+
+    private static long getTotalFeature(String bodyString, String feature) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(bodyString);
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(jsonObject.get("features").toString());
+
+        JSONObject featureObject = (JSONObject) jsonArray.get(0);
+        JSONObject propertiesObject = (JSONObject) featureObject.get("properties");
+        long value = (Long) propertiesObject.get(feature);
+        return value;
     }
 }
+
