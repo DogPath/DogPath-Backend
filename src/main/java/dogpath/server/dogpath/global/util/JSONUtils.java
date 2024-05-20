c1f4c1e2c0b4fa7d980a6acb04fdb31c0c833a84
Han Ju Kim <hjkim2902@gmail.com>
2024-05-20 14:53:27 +0900

    refactor : 프로젝트 구조 변경

--- src/main/java/dogpath/server/dogpath/global/util/JSONUtils.java
+++ src/main/java/dogpath/server/dogpath/global/util/JSONUtils.java
@@ -1,6 +1,5 @@
-package dogpath.server.dogpath.domain.path.algorithm;
+package dogpath.server.dogpath.global.util;
 
-import com.squareup.okhttp.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
