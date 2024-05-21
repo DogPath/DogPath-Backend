b39253789b2de91c512600ad2c9a49cb7e29a046
Han Ju Kim <hjkim2902@gmail.com>
2024-05-21 12:49:12 +0900

    feat : 노드 개수 수정

--- src/main/java/dogpath/server/dogpath/domain/path/algorithm/enums/WalkLength.java
+++ src/main/java/dogpath/server/dogpath/domain/path/algorithm/enums/WalkLength.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public enum WalkLength {
     // Value의 의미: 한 변에 포함되는 Node의 개수
     SHORT(5),
-    MEDIUM(10),
-    LONG(20);
+    MEDIUM(7),
+    LONG(8);
 
     private final int value;
 
@@ -16,6 +16,15 @@ public enum WalkLength {
     }
 
     public int getMeter(){
+        if (this == SHORT) {
+            return 1000;
+        }
+        if (this == MEDIUM) {
+            return 2000;
+        }
+        if (this == LONG) {
+            return 4000;
+        }
         return (this.value/5 * 1000);
     }
 }
